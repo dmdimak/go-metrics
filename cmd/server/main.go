1274e1459a616317c7a4152c8b90d66e2295d4af
Dmitrii Kuznetsov <kdk.mail@yandex.ru>
2024-01-25 15:11:54 +0500

    iteration 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,104 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type gauge float64
+type counter int
+
+type MemStorage struct {
+	metrics map[string]interface{} // словарь, где используются строки в качестве ключей и хранятся значения различных типов intreface{}
+}
+
+func main() {
+	storage := NewMemStorage()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/`, func(w http.ResponseWriter, r *http.Request) {
+		handleMetrics(w, r, storage)
+	})
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		metrics: make(map[string]interface{}),
+	}
+}
+
+func handleMetrics(w http.ResponseWriter, r *http.Request, s *MemStorage) {
+	println("Handle Metrics")
+
+	urlParts := strings.Split(strings.TrimPrefix(r.URL.Path, "/update/"), "/")
+
+	if len(urlParts) != 3 {
+		http.Error(w, "Incorrect request", http.StatusNotFound)
+		return
+	}
+
+	// http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
+	metricType := urlParts[0]
+	metricName := urlParts[1]
+	metricValueStr := urlParts[2]
+
+	if metricName == "" {
+		http.Error(w, "Invalid metric name", http.StatusNotFound)
+		return
+	}
+
+	if metricValueStr == "" {
+		http.Error(w, "Invalid metric value", http.StatusNotFound)
+		return
+	}
+
+	var metricValue interface{}
+	var err error
+
+	switch metricType {
+	case "gauge":
+		metricValue, err = strconv.ParseFloat(metricValueStr, 64)
+		println("gauge")
+		println(metricValue)
+		if err != nil {
+			err = fmt.Errorf("Invalid gauge metric value: %s", metricValueStr)
+		}
+
+		s.metrics[metricName] = metricValue.(float64)
+	case "counter":
+		metricValue, err = strconv.ParseInt(metricValueStr, 10, 0)
+		println("counter")
+		println(metricValue)
+		if err != nil {
+			err = fmt.Errorf("Invalid counter metric value: %s", metricValueStr)
+		}
+		prevValue, exists := s.metrics[metricName]
+		if exists {
+			currentValue, ok := prevValue.(int64)
+			if ok {
+				s.metrics[metricName] = prevValue.(int64) + currentValue
+			} else {
+				err = fmt.Errorf("Stored value of counter metrics is not int64")
+			}
+		} else {
+			s.metrics[metricName] = metricValue.(int64)
+		}
+	default:
+		err = fmt.Errorf("Invalid metric type: %s", metricType)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+}
